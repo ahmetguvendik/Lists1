86c0fb601b46ad434828b416b605c2161c48d757
Ahmet Güvendik <ahmetguvendik01@gmail.com>
2025-07-17 11:16:24 +0300

    Initial commit

---
+++ Lists1/Program.cs
@@ -0,0 +1,16 @@
+﻿List<string> guests = new List<string>();
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("Davetli Giriniz");
+    guests.Add(Console.ReadLine()); 
+}
+
+int counter = 1;
+
+foreach (var item in guests)
+{
+    Console.WriteLine("Davetliler");
+    Console.WriteLine($"{counter} ) {item}");
+    counter++;
+}
